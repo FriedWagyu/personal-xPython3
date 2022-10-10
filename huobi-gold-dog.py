7e27a65e8311ec448826dd44e148ed06f5bc916a
FriedWagyu <sai@bowensai.com>
2022-10-11 01:30:10 +0700

    build

--- huobi-gold-dog.py
+++ huobi-gold-dog.py
@@ -6,20 +6,53 @@ from openpyxl import workbook
 rawHuobi = json.loads(requests.get(url = "https://api.huobi.pro/market/tickers").text)
 rawHuobiData = rawHuobi["data"]
 
+# get binance data
+rawBinance = json.loads(requests.get(url = "https://api.binance.com/api/v3/exchangeInfo").text)
+print(rawBinance)
+#rawBinanceData = rawBinance
+
+
+
 # variables
 totalSymbol = 0
 totalUSDT = 0
 totalUSDC = 0
+totalUSDD = 0
 totalBTC = 0
+totalETH = 0
+totalHT = 0
+listAllSymbol = []
 
 # symbol count
 for i in rawHuobiData:
     totalSymbol = totalSymbol + 1
-    if i["symbol"][-4:0] = "usdt":
+    if i["symbol"][-4:] == "usdt":
         totalUSDT = totalUSDT + 1
-    else
-    
-print(rawHuobiData)
+        listAllSymbol.append(i["symbol"][:-4])
+    elif i["symbol"][-4:] == "usdc":
+        totalUSDC = totalUSDC + 1
+        listAllSymbol.append(i["symbol"][:-4])
+    elif i["symbol"][-4:] == "usdd":
+        totalUSDD = totalUSDD + 1
+        listAllSymbol.append(i["symbol"][:-4])
+    elif i["symbol"][-3:] == "btc":
+        totalBTC = totalBTC + 1
+        listAllSymbol.append(i["symbol"][:-3])
+    elif i["symbol"][-3:] == "eth":
+        totalETH = totalETH + 1
+        listAllSymbol.append(i["symbol"][:-3])
+    elif i["symbol"][-2:] == "ht":
+        totalHT = totalHT + 1
+        listAllSymbol.append(i["symbol"][:-2])
+
+# remove duplicate
+listAllSymbolSimple = []
+
+for i in listAllSymbol:
+    if i not in listAllSymbolSimple:
+        listAllSymbolSimple.append(i)
+        
 
 
-print("ok")
+if len(listAllSymbolSimple) != totalUSDC + totalUSDT + totalUSDD + totalBTC + totalETH + totalHT:
+    print("ok")
